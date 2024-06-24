ddc4513df82fbd73e5c99315e01ab7e4e619f28d
name <fedorovn19@gmail.com>
2024-06-24 17:24:47 +0200

    deploys app

--- tests/Contract.test.ts
+++ tests/Contract.test.ts
@@ -1,4 +1,13 @@
-import { Mina, MerkleMap, Field } from 'o1js';
+import {
+  Mina,
+  MerkleMap,
+  Field,
+  PrivateKey,
+  PublicKey,
+  AccountUpdate,
+} from 'o1js';
+
+import { Contract } from '../src/Contract';
 
 const proofsEnabled: boolean = false;
 const enforceTransactionLimits: boolean = true;
@@ -19,8 +28,10 @@ async function startLocalBlockchainClient(
 describe('Testing contract', () => {
   const map: MerkleMap = new MerkleMap();
 
+  let testAccounts: any;
+
   it('test accounts length', async () => {
-    const testAccounts = await startLocalBlockchainClient(
+    testAccounts = await startLocalBlockchainClient(
       proofsEnabled,
       enforceTransactionLimits
     );
@@ -32,6 +43,34 @@ describe('Testing contract', () => {
       Field(
         '22731122946631793544306773678309960639073656601863129978322145324846701682624'
       )
+    );
+  });
+
+  it('deploys app with admin key', async () => {
+    if (proofsEnabled) {
+      await Contract.compile();
+    }
+
+    const ContractPrivateKey: PrivateKey = PrivateKey.random();
+    const zkAppAddress: PublicKey = ContractPrivateKey.toPublicKey();
+    const zkAppInstance: Contract = new Contract(zkAppAddress);
+
+    const pubKey: PublicKey = testAccounts[0];
+
+    let initialBalance = 10_000_000_000;
+
+    const deployTx = await Mina.transaction(pubKey, async () => {
+      AccountUpdate.fundNewAccount(pubKey).send({
+        to: pubKey,
+        amount: initialBalance,
+      });
+      await zkAppInstance.deploy();
+    });
+
+    await deployTx.prove();
+    await deployTx.sign([testAccounts[0].key, ContractPrivateKey]).send();
+    expect(zkAppInstance.admin.get().toBase58()).toBe(
+      testAccounts[0].toBase58()
     );
   });
 });
